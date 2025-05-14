3afc7d1b79a4ff738afaa03573bf651ed009e130
newrav1k <kritsky.academi@gmail.com>
2025-05-14 18:23:00 +0300

    add kafka configuration to order-service

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -29,11 +29,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mapstruct:mapstruct:${mapStructVersion}")
+    implementation("org.springframework.kafka:spring-kafka")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:${mapStructVersion}")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
