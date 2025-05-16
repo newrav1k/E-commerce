6739c7219acd6d6840270de74bbcf2c90f4b7b89
newrav1k <kritsky.academi@gmail.com>
2025-05-16 03:10:24 +0300

    add topics to application.yaml and product command handler

--- product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/consumer/ProductCommandConsumer.java
+++ product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/consumer/ProductCommandConsumer.java
@@ -2,32 +2,55 @@ package ru.mirea.newrav1k.productservice.controller.kafka.consumer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-import ru.mirea.newrav1k.productservice.model.dto.InventoryPayload;
+import org.springframework.transaction.annotation.Transactional;
 import ru.mirea.newrav1k.productservice.model.entity.Inventory;
 import ru.mirea.newrav1k.productservice.service.InventoryService;
-import ru.newrav1k.mirea.core.model.event.SagaOrderCreatedEvent;
+import ru.newrav1k.mirea.core.model.event.SagaOrderCreationEvent;
+import ru.newrav1k.mirea.core.model.event.SagaProductReservationFailedEvent;
+import ru.newrav1k.mirea.core.model.event.SagaProductReservationSuccessEvent;
 
 @Slf4j
 @Component
 @KafkaListener(topics = {
-        "${product-service.kafka.topics.orders-event}",
-        "${product-service.kafka.topics.products-event}"
+        "${product-service.kafka.topics.order-created}",
+        "${product-service.kafka.topics.product-reservation}"
 }, groupId = "product-service")
 @RequiredArgsConstructor
 public class ProductCommandConsumer {
 
+    @Value("${product-service.kafka.topics.product-reservation}")
+    private String productReservationTopic;
+
     private final InventoryService inventoryService;
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
     @KafkaHandler
-    public void processCreationOrder(@Payload SagaOrderCreatedEvent event) {
+    @Transactional(rollbackFor = {Exception.class})
+    public void processCreationOrder(@Payload SagaOrderCreationEvent event) {
         log.info("Received SagaOrderCreatedEvent: {}", event);
-        for (var productId : event.productIds()) {
-            Inventory inventory = this.inventoryService.findInventoryByProductId(productId);
-            log.info("Received Inventory: {}", inventory);
+        try {
+            for (var product : event.products()) {
+                Inventory inventory = this.inventoryService.findInventoryByProductId(product.productId());
+                inventory.reserveQuantity(product.quantity());
+            }
+            SagaProductReservationSuccessEvent sagaProductReservationSuccessEvent = new SagaProductReservationSuccessEvent(event.orderId());
+            this.kafkaTemplate.send(this.productReservationTopic, sagaProductReservationSuccessEvent);
+        } catch (Exception exception) {
+            log.error("Error while processing SagaOrderCreatedEvent", exception);
+            SagaProductReservationFailedEvent sagaProductReservationFailedEvent = new SagaProductReservationFailedEvent(
+                    event.orderId(),
+                    exception.getMessage()
+            );
+            this.kafkaTemplate.send(this.productReservationTopic, sagaProductReservationFailedEvent);
+
+            throw exception;
         }
     }
 
