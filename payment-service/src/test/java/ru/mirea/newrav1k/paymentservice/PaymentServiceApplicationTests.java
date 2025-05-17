68e920e7f4ae9a579ce771da5cc84caa6510d0cb
newrav1k <kritsky.academi@gmail.com>
2025-05-17 19:03:51 +0300

    add payment-service module

---
+++ payment-service/src/test/java/ru/mirea/newrav1k/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.newrav1k.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name="E-Commerce"
 include(":core")
 include(":order-service")
 include(":product-service")
+include(":payment-service")
