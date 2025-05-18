8410f75e5535ba2750d87c85ba93150b05d5db09
newrav1k <kritsky.academi@gmail.com>
2025-05-18 23:36:53 +0300

    update topics name in order-service

--- order-service/src/main/java/ru/newrav1k/github/orderservice/controller/kafka/consumer/OrderCommandConsumer.java
+++ order-service/src/main/java/ru/newrav1k/github/orderservice/controller/kafka/consumer/OrderCommandConsumer.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +18,10 @@ import ru.newrav1k.mirea.core.model.event.SagaProductReservationSuccessEvent;
 @Slf4j
 @Component
 @KafkaListener(topics = {
-        "${order-service.kafka.topics.product-reservation}"
+        "${order-service.kafka.topics.product-reserved}",
+        "${order-service.kafka.topics.product-failed}",
+        "${order-service.kafka.topics.payment-processed}",
+        "${order-service.kafka.topics.payment-failed}",
 }, groupId = "${order-service.kafka.group-id}")
 @RequiredArgsConstructor
 public class OrderCommandConsumer {
@@ -64,8 +69,8 @@ public class OrderCommandConsumer {
     }
 
     @KafkaHandler(isDefault = true)
-    public void processDefaultEvent() {
-        log.warn("Unknown event type");
+    public void processUnknownEvent(@Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.warn("Unknown event type from topic: {}", topic);
     }
 
 }
