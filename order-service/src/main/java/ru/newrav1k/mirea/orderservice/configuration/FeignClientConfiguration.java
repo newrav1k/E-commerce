0a6de74232f212c166c9c2e127b10573208e1204
newrav1k <kritsky.academi@gmail.com>
2025-05-19 19:17:31 +0300

    fix docker-compose.yaml and FeignClientConfiguration

--- order-service/src/main/java/ru/newrav1k/mirea/orderservice/configuration/FeignClientConfiguration.java
+++ order-service/src/main/java/ru/newrav1k/mirea/orderservice/configuration/FeignClientConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients
+@EnableFeignClients(basePackages = "ru.newrav1k.mirea.orderservice.service.client")
 public class FeignClientConfiguration {
 
 }
