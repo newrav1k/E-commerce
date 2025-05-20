fad0bb5e5c2da1f8a8537f45080d098a67de3914
newrav1k <kritsky.academi@gmail.com>
2025-05-20 22:19:57 +0300

    fix payment transaction method

--- payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/model/enums/PaymentTransactionMethod.java
+++ payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/model/enums/PaymentTransactionMethod.java
@@ -7,9 +7,9 @@ public enum PaymentTransactionMethod {
     CREDIT_CARD, DEBIT_CARD, BANK_TRANSFER;
 
     public static PaymentTransactionMethod fromString(String method) {
-        for (PaymentTransactionStatus status : PaymentTransactionStatus.values()) {
-            if (status.toString().equalsIgnoreCase(method)) {
-                return PaymentTransactionMethod.valueOf(method);
+        for (PaymentTransactionMethod transactionMethod : PaymentTransactionMethod.values()) {
+            if (transactionMethod.toString().equalsIgnoreCase(method)) {
+                return transactionMethod;
             }
         }
         throw new IllegalArgumentException("Invalid payment transaction method: " + method);
