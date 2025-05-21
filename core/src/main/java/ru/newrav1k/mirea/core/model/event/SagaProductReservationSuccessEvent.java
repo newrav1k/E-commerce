10cedff4065251a27789990ad3438b45bbda605d
newrav1k <kritsky.academi@gmail.com>
2025-05-21 13:49:24 +0300

    update saga events

--- core/src/main/java/ru/newrav1k/mirea/core/model/event/SagaProductReservationSuccessEvent.java
+++ core/src/main/java/ru/newrav1k/mirea/core/model/event/SagaProductReservationSuccessEvent.java
@@ -1,10 +1,12 @@
 package ru.newrav1k.mirea.core.model.event;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record SagaProductReservationSuccessEvent(
         UUID orderId,
-        UUID customerId
+        UUID customerId,
+        BigDecimal total
 ) {
 
 }
