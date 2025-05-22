349b6bad155050b3ec94dce855f8796c93c0c74f
newrav1k <kritsky.academi@gmail.com>
2025-05-22 22:52:57 +0300

    add event processes to product command consumer and fix naming from product command producer

--- product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/consumer/ProductCommandConsumer.java
+++ product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/consumer/ProductCommandConsumer.java
@@ -1,20 +1,27 @@
 package ru.mirea.newrav1k.productservice.controller.kafka.consumer;
 
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.dao.TransientDataAccessException;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import ru.mirea.newrav1k.productservice.controller.kafka.producer.ProductCommandProducer;
-import ru.mirea.newrav1k.productservice.model.entity.Inventory;
 import ru.mirea.newrav1k.productservice.service.InventoryService;
-import ru.newrav1k.mirea.core.model.event.SagaCreationCancelledEvent;
+import ru.newrav1k.mirea.core.model.event.SagaOrderCancelledEvent;
 import ru.newrav1k.mirea.core.model.event.SagaOrderCreationEvent;
 import ru.newrav1k.mirea.core.model.event.SagaPaymentFailureEvent;
+import ru.newrav1k.mirea.core.model.payload.ItemInformation;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -31,15 +38,20 @@ public class ProductCommandConsumer {
     private final InventoryService inventoryService;
 
     @KafkaHandler
-    @Transactional(rollbackFor = {Exception.class})
-    public void processCreationOrder(@Payload SagaOrderCreationEvent event) {
-        log.info("Received SagaOrderCreatedEvent: {}", event);
-        try {
-            for (var product : event.products()) {
-                Inventory inventory = this.inventoryService.findInventoryByProductId(product.productId());
-                inventory.reserveQuantity(product.quantity());
+    @Retryable(
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            retryFor = {
+                    OptimisticLockException.class,
+                    TransientDataAccessException.class
             }
-            this.productCommandProducer.processSuccessReserved(event.orderId(), event.customerId(), event.total());
+    )
+    @Transactional
+    public void processCreationOrder(@Payload SagaOrderCreationEvent event) {
+        log.info("Received order created event: {}", event);
+        try {
+            reserveProducts(event.products());
+
+            this.productCommandProducer.processSuccessReserved(event.orderId(), event.customerId(), event.products(), event.total());
         } catch (Exception exception) {
             log.error("Error while processing SagaOrderCreatedEvent", exception);
             this.productCommandProducer.processFailureReserved(event.orderId(), event.customerId(), exception.getMessage());
@@ -48,15 +60,21 @@ public class ProductCommandConsumer {
     }
 
     @KafkaHandler
-    @Transactional(rollbackFor = {Exception.class})
-    public void processCancelledOrder(@Payload SagaCreationCancelledEvent event) {
-        log.info("Received SagaProductReservationSuccessEvent: {}", event);
-        try {
-            for (var item : event.products()) {
-                Inventory inventory = this.inventoryService.findInventoryByProductId(item.productId());
-                inventory.unreserveQuantity(item.quantity());
+    @Retryable(
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            retryFor = {
+                    OptimisticLockingFailureException.class,
+                    TransientDataAccessException.class
             }
-            // TODO: компенсацию средств при отмене заказа
+    )
+    @Transactional
+    public void processCancelledOrder(@Payload SagaOrderCancelledEvent event) {
+        log.info("Received order cancelled event: {}", event);
+        try {
+            unreserveProducts(event.products());
+        } catch (IllegalArgumentException exception) {
+            log.error("Error while processing unreserved products", exception);
+            throw exception;
         } catch (Exception exception) {
             log.error("Error while processing SagaProductReservationSuccessEvent", exception);
             throw exception;
@@ -64,14 +82,44 @@ public class ProductCommandConsumer {
     }
 
     @KafkaHandler
+    @Retryable(
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            retryFor = {
+                    OptimisticLockingFailureException.class,
+                    TransientDataAccessException.class
+            }
+    )
+    @Transactional
     public void processFailurePayment(@Payload SagaPaymentFailureEvent event) {
         log.info("Received payment failure event: {}", event);
-        // TODO: обработку сбоя при оплате
+        try {
+            unreserveProducts(event.products());
+
+            this.productCommandProducer.processFailureReserved(event.orderId(), event.customerId(), event.reason());
+        } catch (IllegalArgumentException exception) {
+            log.error("Error while reservation product: {}", event.products(), exception);
+            this.productCommandProducer.processFailureReserved(event.orderId(), event.customerId(), "Reservation failed");
+            throw exception;
+        } catch (Exception exception) {
+            log.error("Error while processing SagaPaymentFailureEvent", exception);
+            throw exception;
+        }
     }
 
     @KafkaHandler(isDefault = true)
-    public void processUnknownEvent(@Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic) {
-        log.warn("Unknown event type from topic: {}", topic);
+    public void processUnknownEvent(@Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+                                    @Payload Object payload) {
+        log.warn("Unknown event type from topic {} with payload {}", topic, payload);
+    }
+
+    private void reserveProducts(List<ItemInformation> items) {
+        items.forEach(item -> this.inventoryService
+                .updateReservation(item.productId(), item.quantity(), true));
+    }
+
+    private void unreserveProducts(List<ItemInformation> items) {
+        items.forEach(item -> this.inventoryService
+                .updateReservation(item.productId(), item.quantity(), false));
     }
 
 }
--- product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/producer/ProductCommandProducer.java
+++ product-service/src/main/java/ru/mirea/newrav1k/productservice/controller/kafka/producer/ProductCommandProducer.java
@@ -7,8 +7,10 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import ru.newrav1k.mirea.core.model.event.SagaProductReservationFailedEvent;
 import ru.newrav1k.mirea.core.model.event.SagaProductReservationSuccessEvent;
+import ru.newrav1k.mirea.core.model.payload.ItemInformation;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -24,15 +26,15 @@ public class ProductCommandProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void processSuccessReserved(UUID orderId, UUID customerId, BigDecimal total) {
+    public void processSuccessReserved(UUID orderId, UUID customerId, List<ItemInformation> products, BigDecimal total) {
         log.info("Sending product reserved event to topic {}", this.productReservedTopic);
-        SagaProductReservationSuccessEvent event = new SagaProductReservationSuccessEvent(orderId, customerId, total);
+        SagaProductReservationSuccessEvent event = new SagaProductReservationSuccessEvent(UUID.randomUUID(), orderId, customerId, products, total);
         this.kafkaTemplate.send(this.productReservedTopic, event);
     }
 
     public void processFailureReserved(UUID orderId, UUID customerId, String reason) {
         log.info("Sending product failed event to topic {}", this.productFailedTopic);
-        SagaProductReservationFailedEvent event = new SagaProductReservationFailedEvent(orderId, customerId, reason);
+        SagaProductReservationFailedEvent event = new SagaProductReservationFailedEvent(UUID.randomUUID(), orderId, customerId, reason);
         this.kafkaTemplate.send(this.productFailedTopic, event);
     }
 
