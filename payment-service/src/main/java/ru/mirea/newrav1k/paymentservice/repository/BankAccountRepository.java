f2bb8dd605a79bb0f7f3e7bb945a63d39d86b28f
newrav1k <kritsky.academi@gmail.com>
2025-05-22 22:50:13 +0300

    update bank account repository and update service

--- payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/repository/BankAccountRepository.java
+++ payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/repository/BankAccountRepository.java
@@ -1,12 +1,20 @@
 package ru.mirea.newrav1k.paymentservice.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.mirea.newrav1k.paymentservice.model.entity.BankAccount;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ba from BankAccount ba where ba.customerId = :customerId")
+    Optional<BankAccount> findLockByCustomerId(UUID customerId);
 
 }
--- payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/service/BankAccountService.java
+++ payment-service/src/main/java/ru/mirea/newrav1k/paymentservice/service/BankAccountService.java
@@ -2,8 +2,15 @@ package ru.mirea.newrav1k.paymentservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.dao.TransientDataAccessException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.mirea.newrav1k.paymentservice.exception.BankAccountNotFound;
+import ru.mirea.newrav1k.paymentservice.exception.InsufficientFundsException;
+import ru.mirea.newrav1k.paymentservice.model.entity.BankAccount;
 import ru.mirea.newrav1k.paymentservice.model.entity.PaymentTransaction;
 import ru.mirea.newrav1k.paymentservice.model.enums.PaymentTransactionMethod;
 import ru.mirea.newrav1k.paymentservice.model.enums.PaymentTransactionStatus;
@@ -20,40 +27,57 @@ public class BankAccountService {
 
     private final BankAccountRepository bankAccountRepository;
 
+    @Retryable(
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            retryFor = {
+                    OptimisticLockingFailureException.class,
+                    TransientDataAccessException.class
+            }
+    )
     @Transactional
     public void substanceAmount(UUID accountId, UUID orderId, BigDecimal amount) {
         log.info("substance amount {} called", amount);
-        this.bankAccountRepository.findById(accountId).ifPresent(bankAccount -> {
+        BankAccount bankAccount = this.bankAccountRepository.findLockByCustomerId(accountId)
+                .orElseThrow(() -> new BankAccountNotFound("Account not found"));
+        try {
             bankAccount.substanceAmount(amount);
-            bankAccount.addTransaction(
-                    new PaymentTransaction(
-                            bankAccount,
-                            orderId,
-                            PaymentTransactionStatus.SUCCESS,
-                            PaymentTransactionMethod.CREDIT_CARD,
-                            amount,
-                            null
-                    )
-            );
-        });
+            createTransaction(bankAccount, orderId, PaymentTransactionStatus.SUCCESS, amount);
+        } catch (InsufficientFundsException exception) {
+            createTransaction(bankAccount, orderId, PaymentTransactionStatus.FAILED, amount);
+            throw exception;
+        }
     }
 
+    @Retryable(
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            retryFor = {
+                    OptimisticLockingFailureException.class,
+                    TransientDataAccessException.class
+            }
+    )
     @Transactional
     public void depositAmount(UUID accountId, UUID orderId, BigDecimal amount) {
         log.info("deposit amount {} called", amount);
-        this.bankAccountRepository.findById(accountId).ifPresent(bankAccount -> {
-            bankAccount.depositAmount(amount);
-            bankAccount.addTransaction(
-                    new PaymentTransaction(
-                            bankAccount,
-                            orderId,
-                            PaymentTransactionStatus.CANCELLED,
-                            PaymentTransactionMethod.CREDIT_CARD,
-                            amount,
-                            null
-                    )
-            );
-        });
+        BankAccount bankAccount = this.bankAccountRepository.findLockByCustomerId(accountId)
+                .orElseThrow(() -> new BankAccountNotFound("Account not found"));
+
+        bankAccount.depositAmount(amount);
+        createTransaction(bankAccount, orderId, PaymentTransactionStatus.CANCELLED, amount);
+    }
+
+    private void createTransaction(BankAccount bankAccount, UUID orderId,
+                                   PaymentTransactionStatus paymentTransactionStatus, BigDecimal amount) {
+        log.info("binding payment transaction {} called", paymentTransactionStatus);
+        PaymentTransaction paymentTransaction = new PaymentTransaction(
+                bankAccount,
+                orderId,
+                paymentTransactionStatus,
+                PaymentTransactionMethod.CREDIT_CARD,
+                amount,
+                null
+        );
+        bankAccount.addTransaction(paymentTransaction);
+        this.bankAccountRepository.save(bankAccount);
     }
 
 }
