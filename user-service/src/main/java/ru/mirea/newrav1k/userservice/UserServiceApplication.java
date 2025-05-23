892ebe3275b72b7ec41e153f91c668419553ad77
newrav1k <kritsky.academi@gmail.com>
2025-05-23 17:28:20 +0300

    add user-service module

---
+++ user-service/src/main/java/ru/mirea/newrav1k/userservice/UserServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.mirea.newrav1k.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
