43d18a77c6516e6a489edee275351537fc49691b
newrav1k <kritsky.academi@gmail.com>
2025-05-23 15:12:48 +0300

    add notification-service module

---
+++ notification-service/src/test/java/ru/mirea/newrav1k/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.newrav1k.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
