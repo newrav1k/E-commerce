1b5990cf576493edcb0d8aea2e61491fefed1b68
newrav1k <kritsky.academi@gmail.com>
2025-05-23 15:03:14 +0300

    add @EnableRetry to services

--- product-service/src/main/java/ru/mirea/newrav1k/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/ru/mirea/newrav1k/productservice/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package ru.mirea.newrav1k.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
