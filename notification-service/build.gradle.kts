9337c56a28969598a4f6a18e1a4b59505991bc59
newrav1k <kritsky.academi@gmail.com>
2025-05-23 17:14:28 +0300

    update application.yaml and build.gradle.kts in notification-service

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
+
+val springCloudVersion by extra("2024.0.1")
 
 group = "ru.mirea.newrav1k"
 version = "0.0.1-SNAPSHOT"
@@ -24,15 +26,24 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":core"))
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("redis.clients:jedis")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.withType<Test> {
